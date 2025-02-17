c7db94e76c861c23fed987ac36cb0919ca0a9d56
Ijal Fauzi <ijaliffauzi@gmail.com>
2025-02-17 16:52:55 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import IntegritySlider from './components/IntegritySlider'
+
+function App() {
+  return (
+    <div className="w-full">
+      <IntegritySlider />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': ['0.75rem', { lineHeight: '1rem' }],
+        'sm': ['0.875rem', { lineHeight: '1.25rem' }],
+        'base': ['1rem', { lineHeight: '1.5rem' }],
+        'lg': ['1.125rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+        '5xl': ['3rem', { lineHeight: '1.16' }],
+      },
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      keyframes: {
+        slideLeftToRight: {
+          '0%': { 
+            transform: 'translateX(-100%)',
+            opacity: '0'
+          },
+          '100%': { 
+            transform: 'translateX(0)',
+            opacity: '1'
+          }
+        },
+        slideRightToLeft: {
+          '0%': { 
+            transform: 'translateX(100%)',
+            opacity: '0'
+          },
+          '100%': { 
+            transform: 'translateX(0)',
+            opacity: '1'
+          }
+        }
+      },
+      animation: {
+        'slideLeftToRight': 'slideLeftToRight 0.8s cubic-bezier(0.16, 1, 0.3, 1) forwards',
+        'slideRightToLeft': 'slideRightToLeft 0.8s cubic-bezier(0.16, 1, 0.3, 1) forwards'
+      },
+    },
+  },
+  plugins: [],
+}
