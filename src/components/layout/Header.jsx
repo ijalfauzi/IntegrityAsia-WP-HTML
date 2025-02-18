952ec4d80729df59d4efec49a00b424d597d36d3
Ijal Fauzi <ijaliffauzi@gmail.com>
2025-02-18 14:34:32 +0700

    Set max width

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -211,8 +211,8 @@ const Header = () => {
 
   return (
     <header className="relative bg-white shadow-sm" ref={menuRef}>
-      <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-20">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16 lg:h-20">
           {/* Logo */}
           <a href="/" className="flex-shrink-0 py-4">
             <img 
